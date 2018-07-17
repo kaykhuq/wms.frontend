e479643c98918c51c416ea771686aac353ca03b2
qui pham <quipham@asiantechhub.com>
2018-07-17 17:57:12 +0700

    commit code

---
+++ src/components/LayoutComponents/CustomTable/data.js
@@ -0,0 +1,308 @@
+const val=  [
+    { 
+        "gender": "female", 
+        "name": 
+        { 
+            "title": "mademoiselle", 
+            "first": "gudrun", 
+            "last": "henry 4" 
+        }, 
+        "location": 
+        { 
+            "street": "9326 rue courbet", 
+            "city": "castaneda", 
+            "state": "graubünden", 
+            "postcode": 9680, 
+            "coordinates": 
+            { 
+                "latitude": "-50.3415", 
+                "longitude": "112.9724" 
+            }, 
+            "timezone": 
+            { 
+                "offset": "+9:00", 
+                "description": "Tokyo, Seoul, Osaka, Sapporo, Yakutsk" 
+            } 
+        }, 
+        "email": "gudrun.henry@example.com", 
+        "login": 
+        { 
+            "uuid": "664ee726-a46c-4aef-8189-2279d3f13931", 
+            "username": "organicdog261", 
+            "password": "lindsay", 
+            "salt": "pyXN50de", 
+            "md5": "896fe9678e6bc773cd18bd9035612819", 
+            "sha1": "61dedab7584390927f69d9fc28586e613f052078", 
+            "sha256": "80cee9160f52b7396937d55b2fcaca2a7b034cb23a470a3c8bdf647ae435eb8c" 
+        }, 
+        "dob": 
+        { 
+            "date": "1969-09-08T22:14:28Z", 
+            "age": 48 
+        }, 
+        "registered": 
+        { 
+            "date": "2012-01-02T12:18:41Z", 
+            "age": 6 
+        }, 
+        "phone": "(026)-115-7818", 
+        "cell": "(822)-266-7142", 
+        "id": 
+        { 
+            "name": "AVS", 
+            "value": "756.8784.7014.20" 
+        }, 
+        "picture": 
+        { 
+            "large": "https://randomuser.me/api/portraits/women/81.jpg", 
+            "medium": "https://randomuser.me/api/portraits/med/women/81.jpg", 
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/women/81.jpg" 
+        }, 
+        "nat": "CH" 
+    },
+    { 
+        "gender": "female", 
+        "name": 
+        { 
+            "title": "mademoiselle", 
+            "first": "gudrun", 
+            "last": "henry 3" 
+        }, 
+        "location": 
+        { 
+            "street": "9326 rue courbet", 
+            "city": "castaneda", 
+            "state": "graubünden", 
+            "postcode": 9680, 
+            "coordinates": 
+            { 
+                "latitude": "-50.3415", 
+                "longitude": "112.9724" 
+            }, 
+            "timezone": 
+            { 
+                "offset": "+9:00", 
+                "description": "Tokyo, Seoul, Osaka, Sapporo, Yakutsk" 
+            } 
+        }, 
+        "email": "gudrun.henry@example.com", 
+        "login": 
+        { 
+            "uuid": "664ee726-a46c-4aef-8189-2279d3f13931", 
+            "username": "organicdog261", 
+            "password": "lindsay", 
+            "salt": "pyXN50de", 
+            "md5": "896fe9678e6bc773cd18bd9035612819", 
+            "sha1": "61dedab7584390927f69d9fc28586e613f052078", 
+            "sha256": "80cee9160f52b7396937d55b2fcaca2a7b034cb23a470a3c8bdf647ae435eb8c" 
+        }, 
+        "dob": 
+        { 
+            "date": "1969-09-08T22:14:28Z", 
+            "age": 48 
+        }, 
+        "registered": 
+        { 
+            "date": "2012-01-02T12:18:41Z", 
+            "age": 6 
+        }, 
+        "phone": "(026)-115-7818", 
+        "cell": "(822)-266-7142", 
+        "id": 
+        { 
+            "name": "AVS", 
+            "value": "756.8784.7014.20" 
+        }, 
+        "picture": 
+        { 
+            "large": "https://randomuser.me/api/portraits/women/81.jpg", 
+            "medium": "https://randomuser.me/api/portraits/med/women/81.jpg", 
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/women/81.jpg" 
+        }, 
+        "nat": "CH" 
+    },
+    { 
+        "gender": "female", 
+        "name": 
+        { 
+            "title": "mademoiselle", 
+            "first": "gudrun", 
+            "last": "henry 2" 
+        }, 
+        "location": 
+        { 
+            "street": "9326 rue courbet", 
+            "city": "castaneda", 
+            "state": "graubünden", 
+            "postcode": 9680, 
+            "coordinates": 
+            { 
+                "latitude": "-50.3415", 
+                "longitude": "112.9724" 
+            }, 
+            "timezone": 
+            { 
+                "offset": "+9:00", 
+                "description": "Tokyo, Seoul, Osaka, Sapporo, Yakutsk" 
+            } 
+        }, 
+        "email": "gudrun.henry@example.com", 
+        "login": 
+        { 
+            "uuid": "664ee726-a46c-4aef-8189-2279d3f13931", 
+            "username": "organicdog261", 
+            "password": "lindsay", 
+            "salt": "pyXN50de", 
+            "md5": "896fe9678e6bc773cd18bd9035612819", 
+            "sha1": "61dedab7584390927f69d9fc28586e613f052078", 
+            "sha256": "80cee9160f52b7396937d55b2fcaca2a7b034cb23a470a3c8bdf647ae435eb8c" 
+        }, 
+        "dob": 
+        { 
+            "date": "1969-09-08T22:14:28Z", 
+            "age": 48 
+        }, 
+        "registered": 
+        { 
+            "date": "2012-01-02T12:18:41Z", 
+            "age": 6 
+        }, 
+        "phone": "(026)-115-7818", 
+        "cell": "(822)-266-7142", 
+        "id": 
+        { 
+            "name": "AVS", 
+            "value": "756.8784.7014.20" 
+        }, 
+        "picture": 
+        { 
+            "large": "https://randomuser.me/api/portraits/women/81.jpg", 
+            "medium": "https://randomuser.me/api/portraits/med/women/81.jpg", 
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/women/81.jpg" 
+        }, 
+        "nat": "CH" 
+    },
+    { 
+        "gender": "female", 
+        "name": 
+        { 
+            "title": "mademoiselle", 
+            "first": "gudrun", 
+            "last": "henry 2" 
+        }, 
+        "location": 
+        { 
+            "street": "9326 rue courbet", 
+            "city": "castaneda", 
+            "state": "graubünden", 
+            "postcode": 9680, 
+            "coordinates": 
+            { 
+                "latitude": "-50.3415", 
+                "longitude": "112.9724" 
+            }, 
+            "timezone": 
+            { 
+                "offset": "+9:00", 
+                "description": "Tokyo, Seoul, Osaka, Sapporo, Yakutsk" 
+            } 
+        }, 
+        "email": "gudrun.henry@example.com", 
+        "login": 
+        { 
+            "uuid": "664ee726-a46c-4aef-8189-2279d3f13931", 
+            "username": "organicdog261", 
+            "password": "lindsay", 
+            "salt": "pyXN50de", 
+            "md5": "896fe9678e6bc773cd18bd9035612819", 
+            "sha1": "61dedab7584390927f69d9fc28586e613f052078", 
+            "sha256": "80cee9160f52b7396937d55b2fcaca2a7b034cb23a470a3c8bdf647ae435eb8c" 
+        }, 
+        "dob": 
+        { 
+            "date": "1969-09-08T22:14:28Z", 
+            "age": 48 
+        }, 
+        "registered": 
+        { 
+            "date": "2012-01-02T12:18:41Z", 
+            "age": 6 
+        }, 
+        "phone": "(026)-115-7818", 
+        "cell": "(822)-266-7142", 
+        "id": 
+        { 
+            "name": "AVS", 
+            "value": "756.8784.7014.20" 
+        }, 
+        "picture": 
+        { 
+            "large": "https://randomuser.me/api/portraits/women/81.jpg", 
+            "medium": "https://randomuser.me/api/portraits/med/women/81.jpg", 
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/women/81.jpg" 
+        }, 
+        "nat": "CH" 
+    },
+    { 
+        "gender": "female", 
+        "name": 
+        { 
+            "title": "mademoiselle", 
+            "first": "gudrun", 
+            "last": "henry 1" 
+        }, 
+        "location": 
+        { 
+            "street": "9326 rue courbet", 
+            "city": "castaneda", 
+            "state": "graubünden", 
+            "postcode": 9680, 
+            "coordinates": 
+            { 
+                "latitude": "-50.3415", 
+                "longitude": "112.9724" 
+            }, 
+            "timezone": 
+            { 
+                "offset": "+9:00", 
+                "description": "Tokyo, Seoul, Osaka, Sapporo, Yakutsk" 
+            } 
+        }, 
+        "email": "gudrun.henry@example.com", 
+        "login": 
+        { 
+            "uuid": "664ee726-a46c-4aef-8189-2279d3f13931", 
+            "username": "organicdog261", 
+            "password": "lindsay", 
+            "salt": "pyXN50de", 
+            "md5": "896fe9678e6bc773cd18bd9035612819", 
+            "sha1": "61dedab7584390927f69d9fc28586e613f052078", 
+            "sha256": "80cee9160f52b7396937d55b2fcaca2a7b034cb23a470a3c8bdf647ae435eb8c" 
+        }, 
+        "dob": 
+        { 
+            "date": "1969-09-08T22:14:28Z", 
+            "age": 48 
+        }, 
+        "registered": 
+        { 
+            "date": "2012-01-02T12:18:41Z", 
+            "age": 6 
+        }, 
+        "phone": "(026)-115-7818", 
+        "cell": "(822)-266-7142", 
+        "id": 
+        { 
+            "name": "AVS", 
+            "value": "756.8784.7014.20" 
+        }, 
+        "picture": 
+        { 
+            "large": "https://randomuser.me/api/portraits/women/81.jpg", 
+            "medium": "https://randomuser.me/api/portraits/med/women/81.jpg", 
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/women/81.jpg" 
+        }, 
+        "nat": "CH" 
+    }
+]
+export default val;
--- src/pages/MenuPages/ListPage/ListProductPage/ListFirstPage/ListFirst/index.js
+++ src/pages/MenuPages/ListPage/ListProductPage/ListFirstPage/ListFirst/index.js
@@ -1,26 +1,53 @@
 import React from 'react'
 import { Collapse } from 'antd'
 import './style.scss'
+import { Button } from 'antd';
+import { Select } from 'antd';
+import CustomTable from '../../../../../../components/LayoutComponents/CustomTable';
+// import reqwest from 'reqwest';
 
 const Panel = Collapse.Panel
+const Option = Select.Option;
+
+function handleChange(value) {
+    console.log(`selected ${value}`);
+}
 
 class ListFirst extends React.Component {
-  render() {
-    return (
-      <section className="card">
-        <div className="card-header">
-          <div className="utils__title">
-            <strong>List First Page</strong>
-          </div>
-        </div>
-        <div className="card-body">
-          <p>
-            this is list first page
+
+    render() {
+        return (
+            <section className="card">
+                <div className="card-header">
+                    <div className="utils__title">
+                        <div className="wrapper-title">
+                            <span className="icon ml-3 mr-3"><i className="icmn icmn-calendar" aria-hidden="true"></i></span>
+                            <h5 className="page-title mr-3">Danh Sách Hạng Mục Nguyên Liệu Chủ</h5>
+                            <div>
+                                <Button type="primary"><i className="fa fa-plus-square-o" aria-hidden="true"></i> &nbsp;Tạo Mới</Button>
+                            </div>
+                        </div>
+                        <div className="select_filter_number pt-2">
+                            <span className="filter_title">Hiển Thị: </span>
+                            <Select defaultValue="10" style={{ width: 120 }} onChange={handleChange}>
+                                <Option value="10">10</Option>
+                                <Option value="20">20</Option>
+                                <Option value="50">50</Option>
+                                <Option value="100">100</Option>
+                            </Select>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="card-body">
+                    <p>
+                        this is list first page
           </p>
-        </div>
-      </section>
-    )
-  }
+                    <CustomTable />
+                </div>
+            </section>
+        )
+    }
 }
 
 export default ListFirst
